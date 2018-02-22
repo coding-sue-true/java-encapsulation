a9b5065d8d854c66d65ff135440d0a496fd29849
soraia.carmo <coding_sue@icloud.com>
2018-02-22 11:48:27 +0000

    exercised finished

--- encapsulation/src/com/company/Main.java
+++ encapsulation/src/com/company/Main.java
@@ -3,19 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
+//        This is a 'wrong' way of writing code, encapsulation it's not applied. We can easily change variables
+//          values and override them, and that it's not good for a bigger software.
 
-        damage = 11;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
-//        EnhancedPlayer player = new EnhancedPlayer("Tim", 200, "sword");
-//        System.out.println("Initial health is " + player.getHealth());
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+
+
+
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 200, "sword");
+        System.out.println("Initial health is " + player.getHealth());
     }
 }
